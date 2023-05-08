85df5b147f8564883c117bc9197764cde599f5b3
Alexander Mühlhausen <alexander.muehlhausen@sciencemediacenter.de>
2023-05-08 17:09:21 +0200

    zzz.R für kleine Warnings

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+# Lade die folgenden Pakete direkt mit, wenn SMCr geladen wird
+
+#' @import tidyverse
+#' @import lubridate
+#' @import ggplot2
+#' @import dplyr
+#' @import plotly
+#' @import showtext
