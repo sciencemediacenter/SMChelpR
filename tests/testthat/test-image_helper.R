791ac9d685017d6fdb99478018668a288d809555
Alexander Mühlhausen <alexander.muehlhausen@sciencemediacenter.de>
2023-07-14 17:52:11 +0200

    Doesnt work yet, but should work ...

--- tests/testthat/test-image_helper.R
+++ tests/testthat/test-image_helper.R
@@ -5,7 +5,7 @@
 
 
 test_that("image_helper() can save a PNG and save_svg = FALSE works", {
-  unlink("testfiles_image_helper", recursive = TRUE)
+  unlink(file.path(tempdir(), "testfiles_image_helper"), recursive = TRUE)
 
   withr::local_package("ggplot2")
   
@@ -21,24 +21,32 @@ test_that("image_helper() can save a PNG and save_svg = FALSE works", {
   # test that no caption is set
   expect_snapshot(
     cat(
-      image_helper(
-        plot = tmp_ggplot,
-        filename = "testpng",
-        filepath = "testfiles_image_helper",
-        save_svg = FALSE
+      gsub(
+        paste0(
+          toString(tempdir()),
+          "/"
+        ),
+        "",
+        image_helper(
+          plot = tmp_ggplot,
+          filename = "testpng",
+          filepath = file.path(tempdir(), "testfiles_image_helper"),
+          save_svg = FALSE
+        ),
+        fixed = TRUE
       )
     )
   )
 
   # check if png and svg file exist
-  expect_true(file.exists(file.path("testfiles_image_helper", "testpng.png")))
-  expect_true(!file.exists(file.path("testfiles_image_helper", "testpng.svg")))
-  unlink("testfiles_image_helper", recursive = TRUE)
+  expect_true(file.exists(file.path(tempdir(), "testfiles_image_helper", "testpng.png")))
+  expect_true(!file.exists(file.path(tempdir(), "testfiles_image_helper", "testpng.svg")))
+  unlink(file.path(tempdir(), "testfiles_image_helper"), recursive = TRUE)
 })
 
 
 test_that("image_helper() does show the caption and can save SVGs", {
-  unlink("testfiles_image_helper", recursive = TRUE)
+  unlink(file.path(tempdir(), "testfiles_image_helper"), recursive = TRUE)
   withr::local_package("ggplot2")
   
   # create lineplot with points in SMC-CI
@@ -56,16 +64,16 @@ test_that("image_helper() does show the caption and can save SVGs", {
       image_helper(
         plot = tmp_ggplot,
         filename = "testfile",
-        filepath = "testfiles_image_helper",
+        filepath = file.path(tempdir(), "testfiles_image_helper"),
         caption = "Dies ist ein Test"
       )
     )
   )
     
   # check if png and svg file exist
-  expect_true(file.exists(file.path("testfiles_image_helper", "testfile.png")))
-  expect_true(file.exists(file.path("testfiles_image_helper", "testfile.svg")))
-  unlink("testfiles_image_helper", recursive = TRUE)
+  expect_true(file.exists(file.path(tempdir(), "testfiles_image_helper", "testfile.png")))
+  expect_true(file.exists(file.path(tempdir(), "testfiles_image_helper", "testfile.svg")))
+  unlink(file.path(tempdir(), "testfiles_image_helper"), recursive = TRUE)
 })
 
 test_that("image_helper() test plotly, csv_opt and extra_html_tags params", {
@@ -84,7 +92,7 @@ test_that("image_helper() test plotly, csv_opt and extra_html_tags params", {
       image_helper(
         plot = tmp_ggplot,
         filename = "testfile",
-        filepath = "testfiles_image_helper",
+        filepath = file.path(tempdir(), "testfiles_image_helper"),
         caption = "Dies ist ein Test",
         plotly = FALSE,
         csv_opt = FALSE,
@@ -92,5 +100,5 @@ test_that("image_helper() test plotly, csv_opt and extra_html_tags params", {
       )
     )
   )
-  unlink("testfiles_image_helper", recursive = TRUE)
+  unlink(file.path(tempdir(), "testfiles_image_helper"), recursive = TRUE)
 })
