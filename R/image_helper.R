eb5310fe44476fe68d8c9d832e62f22e018f9fdc
lbajiSMC <laura.bahamonjimenez@sciencemediacenter.de>
2023-09-21 11:10:27 +0200

    added image_helper_light()

--- R/image_helper.R
+++ R/image_helper.R
@@ -2,28 +2,36 @@
 #'
 #' Function to create generic image captions in dynamic documents.
 #' For this purpose, HTML code is created, which contains a link to the data needed to create this figure. 
-#' Also includes links to download the figure as a PNG or SVG. 
+#' Also includes links to download the figure in different formats.
 #' 
-#' The image_helper() function returns a list containing the file path, the title including links, 
+#' The functions image_helper_light(), image_helper() return a list containing the file path, the title including links, 
 #' an "alt" text (consisting of the image name) and possibly other HTML tags.
 #' 
 #' @param plot ggplot-object: The ggplot-object to be used for static images.
 #' @param filename string: The name of the image file, without file extension.
-#' @param filepath file.path: The path to the folder in which the image file should be saved. CSV must be placed manually, PNG and SVG are stored automatically.
+#' @param filepath file.path: The path to the folder in which the image file should be saved. (For image_helper(), CSV must be placed manually, PNG and SVG are stored automatically).
 #' @param extra_html_tags string: Between \'\' further HTML-Params can be added, e.g. \'width = \"100%\" data-zoom=\"1\"\'
 #' @param csv_opt boolean, FALSE: no link to a CSV-Download will be provided; default: TRUE.
 #' @param plotly boolean: FALSE, a PNG of the provided ggplot-object will be displayed. TRUE: no figure is shown, plotly-figures must be displayed manually bforehand. Default: FALSE
 #' @param caption string: If not empty: text for the caption, e.g. sources.
 #' @param save_svg boolean: Generation of the corresponding svg. Default: TRUE. For very detailed graphics, you should refrain from creating a sv-graphic, since the file size can become too large.
+#' @param captions vector: A vector with all the captions for the plot.
+#' @param data tibble: A dataframe with two columns 1) fileformat, and 2) filename_suffix. This dataframe is used to generate a link to each data set needed to generate the plot, usually there is one data set per plot, but sometimes there are more.
+#' @param images tibble: A dataframe with two columns 1) fileformat, and 2) filename_suffix. This dataframe is used to generate links to files storing the plot in different image formats.
 #' @return Invisible: HTML code for image caption
 #' @examples
 #' library(ggplot2)
 #' library(dplyr)
 #' tmp_ggplot <- ggplot(data = mpg, aes(x = displ, y = hwy))
-#' tmp_ggplot %>% image_helper("MPG", file.path(tempdir()))
+#' tmp_ggplot %>% image_helper("MPG", file.path(tempdir())) 
+#' filename <- "my_file_name"
+#' filepath <- dirname(rstudioapi::getSourceEditorContext()$path)
+#' data <- tibble(fileformat = c("csv", "json"), filename_suffix = c("", "extra_json_suffix"))
+#' images <- tibble(fileformat = c("html"), filename_suffix = c(""))
+#' captions = c("my_caption_1", "my_caption_2")
+#' image_helper_light(captions, filename, filepath, data, images)
+
 #' @export image_helper
-
-
 image_helper <-
   function(plot,
            filename,
@@ -106,3 +114,110 @@ image_helper <-
     cat(HTML_text)
     invisible(HTML_text)
   }
+
+process_data_tibble <- function(data, filename, filepath, type) {
+  columns <- names(data)
+  column_length <- length(columns)
+  correct_column_names <- ("fileformat" %in% columns & "filename_suffix" %in% columns)
+  if (column_length != 2 | !correct_column_names) {
+    message <- paste("Parameter", type, "should have two columns only. Their names should be: 'fileformat', and 'filename_suffix'")
+    stop(message)
+  }
+  
+  data <- data %>%
+    mutate(
+      path = if_else(
+        filename_suffix != "", 
+        file.path(filepath, paste0(filename, "_", filename_suffix, ".", fileformat)), # paste0 because sep must be ""
+        file.path(filepath, paste0(filename, ".", fileformat)) # paste0 because sep must be ""
+      ),
+      file_exists = file.exists(path),
+      download_link = if_else(
+        file_exists,
+        paste0( # paste0 because sep must be ""
+          '<a href ="',
+          path,
+          '" download>',
+          filename_suffix,
+          ' Als ',
+          toupper(fileformat),
+          '.</a>'
+        ),
+        NA
+      )
+    )
+  
+  if (!all(data$file_exists)) {
+    message <- paste("Data files missing: check if data files exist or correct parameter", type, "in image_helper_light()")
+    stop(message)
+  }
+  
+  # data_download_links <- apply(data, 1, create_links)
+  data_download_links <- data |>
+    filter(!is.na(download_link)) |>
+    pull(download_link)
+  
+  data_download_links_str <- paste(data_download_links, collapse=" ") # mind collapse parameter!! else as many html_texts as links
+  
+  data_download_links_str
+}
+
+#' @export image_helper_light
+#' @rdname image_helper
+image_helper_light <- function(
+    captions,
+    filename=NULL,
+    filepath=NULL,
+    data=NULL,
+    images=NULL
+) {
+  
+  # Create HTML code
+  HTML_text <- "<center>"
+  
+  caption_str <- paste(captions, collapse=" | ")
+  HTML_text <- HTML_text |>
+    paste0( # paste0 because sep must be ""
+      "<small>",
+      caption_str,
+      "</small> <br>"
+    )
+  
+  if (!is.null(data)) {
+    if (is.null(filename) | is.null(filepath)) {
+      stop("Parameters filename and filepath are required if data parameter is passed to image_helper_light()")
+    }
+    
+    HTML_text <- HTML_text |>
+      paste0("Die Daten zur Erstellung dieser Abbildung herunterladen: ") # paste0 because sep must be ""
+    
+    # make paths for all data formats
+    data_download_links_str <- process_data_tibble(data, filename, filepath, "data")
+    
+    HTML_text <- HTML_text |>
+      paste(data_download_links_str, "<br>") # paste because sep must be " "
+    
+  }
+  
+  if (!is.null(images)) {
+    if (is.null(filename) | is.null(filepath)) {
+      stop("Parameters filename and filepath are required if images parameter is passed to image_helper_light()")
+    }
+    
+    HTML_text <- HTML_text |>
+      paste0("Diese Abbildung herunterladen: ") # paste0 because sep must be ""
+    
+    # make paths for all image formats
+    images_download_links_str <- process_data_tibble(images, filename, filepath, "images")
+    
+    HTML_text <- HTML_text |>
+      paste(images_download_links_str) # paste0 because sep must be " "
+    
+  }
+  
+  HTML_text <- HTML_text |>
+    paste("</center> <br>")
+  
+  cat(HTML_text)
+  invisible(HTML_text)
+}
--- tests/testthat/test-image_helper.R
+++ tests/testthat/test-image_helper.R
@@ -124,3 +124,149 @@ test_that("image_helper() test plotly and csv_opt params", {
   )
   unlink(file.path(tempdir(), "testfiles_image_helper"), recursive = TRUE)
 })
+
+##############################
+## image_helper_light tests ##
+##############################
+
+test_directory <- file.path(tempdir(), "testfiles_image_helper_light")
+dir.create(test_directory, showWarnings = FALSE)
+
+test_that("image_helper_light() requires only captions, all other parameters are optional", {
+  helper_output <- image_helper_light(c("my_first_caption", "my_second_caption"))
+  expected_output <- "<center><small>my_first_caption | my_second_caption</small> <br> </center> <br>"
+  
+  expect_identical(helper_output, expected_output)
+})
+
+test_that("image_helper_light() works for parameter data independently of parameter images", {
+  withr::local_package("readr")
+  
+  filename <- "test_data"
+  data <- tibble(
+    fileformat = c("csv"),
+    filename_suffix = c("")
+  )
+  captions = c("my_caption")
+  
+  datapath <- file.path(test_directory, paste0(filename, ".csv"))
+  df <- tibble(col1=c(1,2), col2=c(3,4))
+  readr::write_csv(df, datapath)
+  
+  expect_no_error(
+    image_helper_light(
+      filename=filename,
+      filepath=test_directory,
+      data=data,
+      captions=captions
+    )
+  )
+})
+
+test_that("image_helper_light() works for parameter images independently of parameter data", {
+  withr::local_package("ggplot2")
+  
+  filename <- "test_image"
+  images <- tibble(
+    fileformat = c("png"),
+    filename_suffix = c("")
+  )
+  captions = c("my_caption")
+  
+  pngpath <- file.path(test_directory, paste0(filename, ".png"))
+  tmp_ggplot <-
+    ggplot(data = mpg, aes(x = displ, y = hwy, color = class)) +
+    geom_line() +
+    geom_point() +
+    labs(title = "Beispiel-Plot im SMC-Theme - Schriftart: txyz") +
+    SMC_theme_ggplot()
+  
+  ggsave(plot = tmp_ggplot,
+         filename = pngpath,
+         device = "png")
+  
+  expect_no_error(
+    image_helper_light(
+      filename=filename,
+      filepath=test_directory,
+      images=images,
+      captions=captions
+    )
+  )
+})
+
+test_that("image_helper_light() throws error if parameters data or images are passed without filepath and filename", {
+  data <- tibble(
+    fileformat = c("csv"),
+    filename_suffix = c("")
+  )
+  captions = c("my_caption")
+  
+  expect_error(
+    image_helper_light(
+      data=data,
+      captions=captions
+    ),
+    "Parameters filename and filepath are required if data parameter is passed to image_helper_light()"
+  )
+})
+
+test_that("image_helper_light() throws error if data' or images' column names differ from 'fileformat' and 'filename_suffix'", {
+  filename <- "test_file"
+  data <- tibble(
+    another_name = c("csv"),
+    filename_suffix = c("")
+  )
+  captions = c("my_caption")
+  
+  expect_error(
+    image_helper_light(
+      filename=filename,
+      filepath=test_directory,
+      data=data,
+      captions=captions
+    ),
+    "Parameter data should have two columns only. Their names should be: 'fileformat', and 'filename_suffix'"
+  )
+})
+
+test_that("image_helper_light() throws error if data or images have more than two columns", {
+  filename <- "test_file"
+  data <- tibble(
+    fileformat = c("csv"),
+    filename_suffix = c(""),
+    third_col = c("bla")
+  )
+  captions = c("my_caption")
+  
+  expect_error(
+    image_helper_light(
+      filename=filename,
+      filepath=test_directory,
+      data=data,
+      captions=captions
+    ),
+    "Parameter data should have two columns only. Their names should be: 'fileformat', and 'filename_suffix'"
+  )
+})
+
+test_that("image_helper_light() thows error if data or image file path does not exist", {
+  filename <- "test_file_2"
+  data <- tibble(
+    fileformat = c("csv"),
+    filename_suffix = c("")
+  )
+  captions = c("my_caption")
+  
+  expect_error(
+    image_helper_light(
+      filename=filename,
+      filepath=test_directory,
+      data=data,
+      captions=captions
+    ),
+    "Data files missing: check if data files exist or correct parameter data in image_helper_light()"
+  )
+})
+
+unlink(test_directory, recursive = TRUE)
