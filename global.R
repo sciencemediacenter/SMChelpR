0a1973fd9919e0eb33be3001a83c2ff4ba96602a
Simon Essink <simon.essink@sciencemediacenter.de>
2025-08-07 08:43:22 +0200

    Bump version number

--- global.R
+++ global.R
@@ -19,7 +19,7 @@ setwd("..")
 system("R CMD build SMChelpR --resave-data")
 
 # verify current version
-system("R CMD check SMChelpR_0.1-3.tar.gz --as-cran")
+system("R CMD check SMChelpR_0.1-4.tar.gz --as-cran")
 
 ###############################################
 ## Install locally to test for hidden errors ##
